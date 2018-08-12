c57268ed54b9d6e2ea4b48eec564a4541e939314
reymesson1 <reymesson1>
2018-08-12 16:14:16 -0400

    detail check

--- webPaa1035/my-app/src/app/master/view.component.ts
+++ webPaa1035/my-app/src/app/master/view.component.ts
@@ -13,14 +13,10 @@ export class ViewComponent {
 
   constructor(private route:ActivatedRoute,private data: RestSourceData){
     this.data.getMessage();
-    this.masterData = {"author":"5ae1a7105a926e0f84230d61","date":"8/11/2018 6:06:23 AM","description":"test",
-      "position":"1533981983874"};
-    console.log(this.data.messages);
   }
 
   ngOnInit(){
     if(this.data.messages.length>0){
-
       this.position = this.route.snapshot.params['id'];
       let filteredData = this.data.messages.filter(
         (master) => master.position.indexOf(this.position) !== -1
